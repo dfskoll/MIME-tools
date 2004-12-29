f9606c19b55d95da65a7a5458657105bb2fbe4c8
David Skoll <dfs@roaringpenguin.com>
2004-12-29 17:50:40 +0000

    Prepare for 5.416 release. Patch from Julian Field to strip trailing whitespace in ParamVal.pm

--- lib/MIME/Entity.pm
+++ lib/MIME/Entity.pm
@@ -249,7 +249,7 @@ use IO::Lines;
 #------------------------------
 
 ### The package version, both in 1.23 style *and* usable by MakeMaker:
-$VERSION = "5.415.1";
+$VERSION = "5.416";
 
 ### Boundary counter:
 my $BCount = 0;
