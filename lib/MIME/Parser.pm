74b764a02f7bd30aeae73add9308e03976ce52fd
Dave O'Neill <dmo@roaringpenguin.com>
2007-09-20 17:08:40 -0400

    Update to version 5.421 for release

--- lib/MIME/Parser.pm
+++ lib/MIME/Parser.pm
@@ -180,7 +180,7 @@ package MIME::Parser;
 #------------------------------
 
 ### The package version, both in 1.23 style *and* usable by MakeMaker:
-$VERSION = "5.420_03";
+$VERSION = "5.421";
 
 ### How to catenate:
 $CAT = '/bin/cat';
