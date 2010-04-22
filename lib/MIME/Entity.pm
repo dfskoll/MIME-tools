e5391d3661db845bb74d9d65c3c685d1aa05c8dd
Dave O'Neill <dmo@roaringpenguin.com>
2010-04-22 11:00:48 -0400

    Prep for 5.428 release

--- lib/MIME/Entity.pm
+++ lib/MIME/Entity.pm
@@ -246,7 +246,7 @@ use IO::Lines;
 #------------------------------
 
 ### The package version, both in 1.23 style *and* usable by MakeMaker:
-$VERSION = "5.427";
+$VERSION = "5.428";
 
 ### Boundary counter:
 my $BCount = 0;
